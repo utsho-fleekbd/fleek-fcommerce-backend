783742b17ec63c0b183b56af5013e0af527466f5
Utsho MH <utshomh.dev@gmail.com>
2026-03-02 22:27:21 +0600

    added upload image and edit profile

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import cors from "cors";
+import path from "path";
 import express from "express";
 
 import authRouter from "./modules/auth/auth.router";
@@ -7,6 +8,8 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/uploads", express.static(path.join(__dirname, "../uploads")));
 
 app.use("/auth", authRouter);
 
